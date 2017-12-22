b67dc184bda3668533f1118681da5558f5980424
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2017-12-22 19:48:11 -0200

    Setup initial folder structure and remove useless files

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/app.scss';
-import App from './App';
+import AppContainer from './containers/AppContainer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
 registerServiceWorker();
