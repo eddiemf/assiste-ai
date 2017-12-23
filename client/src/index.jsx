3693afab1b4415c9c46b82957e88ed0974d28c5f
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2017-12-23 00:22:10 -0200

    Install and apply redux thunk to handle the async actions

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 import reducer from './reducers';
 import './styles/app.scss';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>
