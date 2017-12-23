6e52ebf7bbf9696017882b31ac8b380d2020cae0
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2017-12-23 02:03:39 -0200

    Check if there's a token set in local storage before the app renders to manage the login state

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,15 +4,21 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
-import App from './components/App';
+import AppContainer from './containers/AppContainer';
 import reducer from './reducers';
+import { loginUserSuccess } from './actions/auth';
 import './styles/app.scss';
 
 const store = createStore(reducer, applyMiddleware(thunk));
+const token = localStorage.getItem('token');
+
+if (token !== null) {
+  store.dispatch(loginUserSuccess(token));
+}
 
 render(
   <Provider store={store}>
-    <App />
+    <AppContainer />
   </Provider>,
   document.getElementById('root'),
 );
