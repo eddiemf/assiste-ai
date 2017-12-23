f7978c043b87eded7612564b523edf526c84b3a3
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2017-12-22 23:21:10 -0200

    Create a test for the auth reducer, install deepfreeze library for the tests, and update the .eslintrc to allow the expect environment

---
+++ client/src/reducers/auth.spec.js
@@ -0,0 +1,94 @@
+import jwt from 'jsonwebtoken';
+import deepfreeze from 'deep-freeze';
+import auth from './auth';
+import * as actions from '../actions/auth';
+import * as types from '../constants/actionTypes';
+
+describe('Auth reducer', () => {
+  const initialState = {
+    token: null,
+    userName: null,
+    userPicture: null,
+    isAuthenticated: false,
+    isAuthenticating: false,
+    statusText: null,
+  };
+
+  deepfreeze(initialState);
+
+  it('should handle the login request', () => {
+    const state = { ...initialState };
+    const action = actions.loginUserRequest();
+
+    deepfreeze(state);
+    deepfreeze(action);
+
+    expect(auth(state, action)).toMatchObject({
+      ...state,
+      isAuthenticating: true,
+    });
+  });
+
+  it('should handle the user login', () => {
+    const state = { ...initialState, isAuthenticating: true };
+    const payload = {
+      userName: 'John',
+      userPicture: 'https://www.google.com/',
+    };
+    const token = jwt.sign(payload, '123');
+    const action = actions.loginUserSuccess(token);
+
+    deepfreeze(state);
+    deepfreeze(action);
+
+    expect(auth(state, action)).toMatchObject({
+      ...state,
+      token,
+      userName: 'John',
+      userPicture: 'https://www.google.com/',
+      isAuthenticated: true,
+      isAuthenticating: false,
+      statusText: 'Autenticado com sucesso!',
+    });
+  });
+
+  it('should handle the login failure', () => {
+    const state = { ...initialState, token: 'Random token' };
+    const action = actions.loginUserFailure();
+
+    deepfreeze(state);
+    deepfreeze(action);
+
+    expect(auth(state, action)).toMatchObject({
+      ...state,
+      token: null,
+      isAuthenticated: false,
+      isAuthenticating: false,
+      statusText: 'Erro ao autenticar.',
+    });
+  });
+
+  it('should handle the user logout', () => {
+    const state = {
+      ...initialState,
+      token: 'Random token',
+      userName: 'John',
+      userPicture: 'https://www.google.com/',
+      isAuthenticated: true,
+      statusText: 'Autenticado com sucesso!',
+    };
+    const action = actions.logout();
+
+    deepfreeze(state);
+    deepfreeze(action);
+
+    expect(auth(state, action)).toMatchObject({
+      ...state,
+      token: null,
+      userName: null,
+      userPicture: null,
+      isAuthenticated: false,
+      statusText: null,
+    });
+  });
+});
