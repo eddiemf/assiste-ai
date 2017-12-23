434f197c17af2d0e28d65f441eeb043fe1ce3540
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2017-12-23 01:59:48 -0200

    Make the login action make a real request

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,11 +4,6 @@ import { ENDPOINTS } from '../constants/api';
 
 export const loginUserRequest = () => ({ type: LOGIN_USER_REQUEST });
 
-export const login = (email, password) => (dispatch) => callApi(ENDPOINTS.login)
-  .then(({ data }) => data)
-  .then(({ token }) => dispatch(loginUserSuccess(token)))
-  .catch(() => dispatch(loginUserFailure()));
-
 export const loginUserSuccess = token => ({
   type: LOGIN_USER_SUCCESS,
   payload: { token },
@@ -17,3 +12,16 @@ export const loginUserSuccess = token => ({
 export const loginUserFailure = () => ({ type: LOGIN_USER_FAILURE });
 
 export const logout = () => ({ type: LOGOUT_USER });
+
+export const login = (email, password) => dispatch => callApi(ENDPOINTS.login, {
+  method: 'post',
+  data: { email, password },
+}).then(({ data }) => data)
+  .then(({ token }) => {
+    if (token) {
+      return dispatch(loginUserSuccess(token));
+    }
+
+    return dispatch(loginUserFailure());
+  })
+  .catch(() => dispatch(loginUserFailure()));
