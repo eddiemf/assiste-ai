8310fd3e2caa38653e85cd2400bef90cb6dbad4f
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2017-12-22 22:19:50 -0200

    Alter User reducer to Auth reducer and add its methods for authentication

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,47 @@
+import jwtDecode from 'jwt-decode';
+import { LOGIN_USER_REQUEST, LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE, LOGOUT_USER } from '../constants/actionTypes';
+import { createReducer } from '../utils';
+
+const initialState = {
+  token: null,
+  userName: null,
+  userPicture: null,
+  isAuthenticated: false,
+  isAuthenticating: false,
+  statusText: null,
+};
+
+export default createReducer(initialState, {
+  [LOGIN_USER_REQUEST]: state => ({
+    ...state,
+    isAuthenticating: true,
+    statusText: null,
+  }),
+
+  [LOGIN_USER_SUCCESS]: (state, payload) => ({
+    ...state,
+    token: payload.token,
+    userName: jwtDecode(payload.token).userName,
+    userPicture: jwtDecode(payload.token).userPicture,
+    isAuthenticated: true,
+    isAuthenticating: false,
+    statusText: 'Autenticado com sucesso!',
+  }),
+
+  [LOGIN_USER_FAILURE]: state => ({
+    ...state,
+    token: null,
+    isAuthenticated: false,
+    isAuthenticating: false,
+    statusText: 'Erro ao autenticar.',
+  }),
+
+  [LOGOUT_USER]: state => ({
+    ...state,
+    token: null,
+    userName: null,
+    userPicture: null,
+    isAuthenticated: false,
+    statusText: null,
+  }),
+});
