90ae7038aab15d999802a7474e6f6bfbaf5f2913
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2017-12-27 21:29:24 -0200

    Refactor app header component to be presentational

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,42 +1,43 @@
-/* eslint-disable */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import logo from '../images/logo.svg';
 import defaultProfilePicture from '../images/profile.jpg';
 
 const propTypes = {
-  userName: PropTypes.string.isRequired,
-  userPicture: PropTypes.string.isRequired,
-  isAuthenticated: PropTypes.bool.isRequired,
+  auth: PropTypes.shape({
+    userName: PropTypes.string.isRequired,
+    userPicture: PropTypes.string.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
+  }).isRequired,
   logout: PropTypes.func.isRequired,
 };
 
-class AppHeader extends Component {
-  render() {
-    const { userName, userPicture, isAuthenticated } = this.props.auth;
+const AppHeader = (props) => {
+  const { userName, userPicture, isAuthenticated } = props.auth;
 
-    return (
-      <header className="app-header">
-        <div className="app-header__main-bar">
-          <div className="container">
-            <div className="app-header__main-bar-inner">
-              <div className="app-header__logo">
-                <img src={logo} alt="Logotipo" />
-              </div>
-
-              <nav className="app-header__user-nav">
-                <p>{userName} <button onClick={this.props.logout}>Logout</button></p>
-                <div className="app-header__user-picture">
-                  <img src={userPicture || defaultProfilePicture} alt="Imagem do perfil"/>
-                </div>
-              </nav>
+  return (
+    <header className="app-header">
+      <div className="app-header__main-bar">
+        <div className="container">
+          <div className="app-header__main-bar-inner">
+            <div className="app-header__logo">
+              <img src={logo} alt="Logotipo" />
             </div>
+
+            <nav className="app-header__user-nav">
+              {isAuthenticated && (
+                <p>{userName} <button onClick={props.logout}>Logout</button></p>
+              )}
+              <div className="app-header__user-picture">
+                <img src={userPicture || defaultProfilePicture} alt="Imagem do perfil" />
+              </div>
+            </nav>
           </div>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
 
 AppHeader.propTypes = propTypes;
 
--- client/src/containers/AppHeaderContainer.jsx
+++ client/src/containers/AppHeaderContainer.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AppHeader from '../components/AppHeader';
-import auth from '../reducers/auth';
-import {
-  logout,
-} from '../actions/auth';
+import { logout } from '../actions/auth';
 
 const AppHeaderContainer = props => <AppHeader {...props} />;
 
 const mapStateToProps = state => ({
-  auth: auth(state.auth),
+  auth: state.auth,
 });
 
-const mapDispatchToProps = {
+export default connect(mapStateToProps, {
   logout,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AppHeaderContainer);
+})(AppHeaderContainer);
